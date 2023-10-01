355f4610c238f7a39fda203249e6e1e0d04844c1
ALIUSER <aliuser@aliVirtualB.myguest.virtualbox.org>
2023-10-01 12:50:44 -0500

    new files

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,25 @@
+$(() => {
+  $('input').css({ 'margin-right': '10px' });
+  const listAmt = {};
+  $("input[type='checkbox']").each(function () {
+    const amId = $(this).data('id');
+    const amNa = $(this).data('name');
+
+    if ($(this).prop('checked')) {
+      listAmt[amId] = amNa;
+    } else {
+      delete listAmt[amId];
+    }
+  });
+  $('.amenities h4').text(Object.values(listAmt).join(', '));
+
+  $('input:checkbox').on('change', function () {
+    if ($(this).prop('checked')) {
+      listAmt[$(this).data('id')] = $(this).data('name');
+      console.log(listAmt);
+    } else {
+      delete listAmt[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(listAmt).join(', '));
+  });
+});
